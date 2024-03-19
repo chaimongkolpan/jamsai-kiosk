26bbb930ea1f15da136adda4b65e333ae30d68d3
Chaimongkol <cpanawasri@gmail.com>
2024-03-19 21:31:52 +0700

    allow all ip

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [VitePWA({ registerType: "autoUpdate" })],
   server: {
+    host: '0.0.0.0',
     port: 3000,
   },
 });
